08f5a09bb443208db697e38185a265eb0114580a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-05-15 08:08:28 -0500

    Actualiza

--- db/structure.sql
+++ db/structure.sql
@@ -1767,7 +1767,8 @@ CREATE TABLE public.cor1440_gen_proyectofinanciero (
     compromisos character varying(5000),
     monto numeric,
     sectorapc_id integer,
-    titulo character varying(1000)
+    titulo character varying(1000),
+    poromision boolean
 );
 
 
@@ -9159,6 +9160,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20210417152053'),
 ('20210419161145'),
 ('20210428143811'),
-('20210430160739');
+('20210430160739'),
+('20210514201449');
 
 
