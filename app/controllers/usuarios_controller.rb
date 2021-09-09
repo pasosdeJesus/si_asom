e4ccce404b60cf1dcdb5475c298359e3c7c7723f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-09 05:11:49 -0500

    Listado de usuarois inicialmente presenta los habilitados

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -5,5 +5,12 @@ require 'sivel2_gen/concerns/controllers/usuarios_controller'
 class UsuariosController < Heb412Gen::ModelosController
   include Sivel2Gen::Concerns::Controllers::UsuariosController
 
+  def index_reordenar(c)
+    if !params || !params[:filtro] || !params[:filtro][:bushabilitado]
+      c = c.where("usuario.fechadeshabilitacion IS NULL")
+    end
+    #c = c.reorder([:apellidos])
+    return c
+  end
 end
 
