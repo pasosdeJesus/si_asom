1a4b0400f13afa5d1bf05748cd6655ba2b63720a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-10-28 10:51:30 -0500

    apexcharts

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -23,21 +23,23 @@ Rails.start()
 import Turbolinks from "turbolinks"
 Turbolinks.start()
 
-import ApexCharts from 'apexcharts'
-window.ApexCharts = ApexCharts
-
 import $ from "expose-loader?exposes=$,jQuery!jquery"
-
-var L = require('leaflet');
-var mc= require('leaflet.markercluster');
-
+import 'jquery-ui'
+import 'jquery-ui/ui/widgets/autocomplete'
+import 'jquery-ui/ui/focusable'
+import 'jquery-ui/ui/data'
+import 'jquery-ui/ui/widgets/tooltip'
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
 import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
-import 'jquery-ui'
-import 'jquery-ui/ui/widgets/autocomplete'
-import 'jquery-ui/ui/focusable'
-import 'jquery-ui/ui/data'
+
+
+// Apexcharts
+import ApexCharts from 'apexcharts'
+window.ApexCharts = ApexCharts
+
+var L = require('leaflet');
+var mc= require('leaflet.markercluster');
 
